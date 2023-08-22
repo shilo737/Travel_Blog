7e4cb72033721b6213fbdee2f7703489752453a9
shilo farajun <mr.shifu737@gmail.com>
2023-08-23 00:13:18 +0300

    ok

--- server/db/mongoConnect.js
+++ server/db/mongoConnect.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const {config} = require('../config/secret')
+require("dotenv").config();
+
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(config.MONGO_DB);
+  await mongoose.connect(process.env.MONGO_DB);
   console.log("mongo atlas connect!");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
